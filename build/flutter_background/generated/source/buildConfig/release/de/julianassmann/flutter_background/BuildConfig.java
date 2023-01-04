e00ef014263fb37447756f77df064065a1912d3f
Ali <alisekerr2018@gmail.com>
2023-01-05 00:12:03 +0300

    First Commit

---
+++ build/flutter_background/generated/source/buildConfig/release/de/julianassmann/flutter_background/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package de.julianassmann.flutter_background;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "de.julianassmann.flutter_background";
+  public static final String BUILD_TYPE = "release";
+}
